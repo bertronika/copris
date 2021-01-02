1c59509128f59081061255576f502b60fd5aa755
bertronika <nejc@bertoncelj.eu.org>
2021-01-02 14:45:57 +0100

    Log date, time and PID only when release is debug

--- src/debug.c
+++ src/debug.c
@@ -8,8 +8,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef REL
 #include <unistd.h> /* getpid() */
 #include <time.h>
+#endif
 
 #include "debug.h"
 
@@ -47,9 +50,11 @@ int log_debug() {
 }
 
 void log_date() {
+#ifdef REL
 	time_t t = time(NULL);
 	struct tm cur_time = *localtime(&t);
 	printf("%02d.%02d.%d %02d:%02d:%02d copris[%d]: ",
 		cur_time.tm_mday, cur_time.tm_mon + 1, cur_time.tm_year + 1900,
 		cur_time.tm_hour, cur_time.tm_min, cur_time.tm_sec, getpid());
+#endif
 }
