ae9568edecc9b109d679a00b5eac8760bd518854
bertronika <nejc@bertoncelj.eu.org>
2021-02-14 01:17:24 +0100

    Rephrase writer debug message, return on error

--- src/writer.c
+++ src/writer.c
@@ -3,11 +3,12 @@
  * Data output interface
  * 
  * Part of COPRIS - a converting printer server
- * (c) 2020 Nejc Bertoncelj <nejc at bertoncelj.eu.org>
+ * (c) 2020-21 Nejc Bertoncelj <nejc at bertoncelj.eu.org>
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "debug.h"
 #include "writer.h"
@@ -19,12 +20,14 @@ int copris_write(char *dest, unsigned char *data) {
 	int ferr;        // Error code of a file operation
 	
 	// Count the buffer
-	for(buf_num = 0; data[buf_num] != '\0'; buf_num++);
+	//for(buf_num = 0; data[buf_num] != '\0'; buf_num++);
+	buf_num = strlen((char *)data);
 	
 	file_ptr = fopen(dest, "a"); // rEAD, wRITE, aPPEND, man 3 fopen
 	
 	if(file_ptr == NULL)
-		log_perr(-1, "fopen", "Failed to open output file for writing/appending.");
+		if(log_perr(-1, "fopen", "Failed to open output file for writing/appending."))
+			return 1;
 		
 	if(log_debug()) {
 		log_date();
@@ -34,16 +37,19 @@ int copris_write(char *dest, unsigned char *data) {
 	buf_written = fwrite(data, sizeof(data[0]), buf_num, file_ptr);
 	if(log_debug()) {
 		log_date();
-		printf("Write size is %d B, written size is %d B.\n", buf_num, buf_written);
+		printf("Finished writing %d B.\n", buf_written);
 	}
+
 	if(buf_written < buf_num) {
 		fprintf(stderr, "fwrite: Failure while writing to output file; " 
 		                "not enough bytes transferred.\n");
-// 		exit(1); -- should we actually terminate the program?
+		return 1;
 	}
 	
 	ferr = fclose(file_ptr);
-	log_perr(ferr, "close", "Failed to close the output file after writing.");
+	if(log_perr(ferr, "close", "Failed to close the output file after writing."))
+		return 1;
+
 	if(log_debug()) {
 		log_date();
 		printf("Output file closed.\n");
