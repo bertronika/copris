bertronika/copris

commit 4773f58e1e10087cb3e0a5fc9282bd84b831bdf3

Make copris_read() return an int on failure

src/server.c
@@ -97,8 +97,8 @@ int copris_listen(int *parentfd, int portno) {
 	return 0;
 }
 
-int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, int printerset,
-                int limitnum, int limit_cutoff) {
+int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile,
+                int printerset, int limitnum, int limit_cutoff) {
 	int fderr;             // Error code of a socket operation
 	int childfd;           // Child socket, which processes one client at a time
 	int bytenum   = 0;     // Received/sent message (byte) size
@@ -115,7 +115,9 @@ int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, 
 
 	// Wait for a connection request and accept it
 	childfd = accept(*parentfd, (struct sockaddr *)&clientaddr, &clientlen);
-	log_perr(childfd, "accept", "Failed to accept the connection.");
+	if(log_perr(childfd, "accept", "Failed to accept the connection."))
+		return 1;
+
 	if(log_info()) {
 		log_date();
 		printf("Connection to socket accepted.\n");
@@ -124,7 +126,8 @@ int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, 
 	// Prevent more than one connection if not a daemon
 	if(!daemon) {
 		fderr = close(*parentfd);
-		log_perr(fderr, "close", "Failed to close the parent connection.");
+		if(log_perr(fderr, "close", "Failed to close the parent connection."))
+			return 1;
 	}
 
 	// Get the hostname of the client
@@ -179,7 +182,7 @@ int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, 
 
 		copris_send(buf, fderr, &destination, printerset, &trfile);
 
-		memset(buf, '\0', BUFSIZE + 1); // Clear the buffer for next read.
+//		memset(buf, '\0', BUFSIZE + 1); // Clear the buffer for next read. TODO ???
 		if(limit_cutoff == 2) {
 			if(log_err())
 				printf("\nClient exceeded send size limit (%d B/%d B), cutting off and "
@@ -187,11 +190,13 @@ int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, 
 			break;
 		}
 	}
-	log_perr(fderr, "read", "Error reading from socket.");
+	if(log_perr(fderr, "read", "Error reading from socket."))
+		return 1;
 
 	// Close the current connection 
 	fderr = close(childfd);
-	log_perr(fderr, "close", "Failed to close the child connection.");
+	if(log_perr(fderr, "close", "Failed to close the child connection."))
+		return 1;
 
 	if(log_err() && !destination->exists)
 		printf("; EOS\n");
@@ -215,7 +220,7 @@ int copris_read(int *parentfd, int daemon, attrib *destination, attrib *trfile, 
 		log_date();
 		printf("Connection from %s (%s) closed.\n", host, hostaddrp);
 	}
-    
+
 	return 0;
 }
 

commit ff282843d4207924c1d9c6580d1c6243d0c993b9

Fix a buffer clearing regression
Introduced in 4773f58, received messages were duplicated every new line.
Now, instead of clearing the whole buffer every time, as it was done
before the regression, a simple NUL terminator is put on end of the
received buffer.

src/server.c
@@ -149,7 +149,10 @@ int copris_read_socket(int *parentfd, struct Attribs *attrib) {
 	// Read the data sent by the client into the buffer
 	while((fderror = read(childfd, buf, BUFSIZE)) > 0) {
 		bytenum += fderror; // Append read bytes to the total byte counter
+
+		// Byte limit handling
 		if(attrib->limitnum && bytenum > attrib->limitnum) {
+			// Cut-off mid-text and terminate later
 			if(attrib->limit_cutoff) {
 				buf[attrib->limitnum] = '\0';
 				discarded = bytenum - attrib->limitnum;
@@ -157,6 +160,7 @@ int copris_read_socket(int *parentfd, struct Attribs *attrib) {
 
 				fderror = write(childfd, limit_message, strlen(limit_message));
 				log_perr(fderror, "write", "Error sending termination text to socket.");
+			// Discard whole line and terminate
 			} else {
 				if(log_err())
 					printf("Client exceeded send size limit (%d B/%d B), discarding remaining "
@@ -169,19 +173,25 @@ int copris_read_socket(int *parentfd, struct Attribs *attrib) {
 
 				break;
 			}
-		}
+		} /* end of byte limit handling */
+
+		// Terminate the buffer after reading completed
+		buf[fderror] = '\0';
 
 // 		copris_send(buf, fderror, &trfile, printerset, &destination);
 		copris_process(buf, fderror, attrib);
 
-//		memset(buf, '\0', BUFSIZE + 1); // Clear the buffer for next read. TODO ???
+// 		memset(buf, '\0', BUFSIZE + 1); // Clear the buffer for next read. TODO ???
+
+		// Terminate connection if cut-off set
 		if(attrib->limit_cutoff == 2) {
 			if(log_err())
 				printf("\nClient exceeded send size limit (%d B/%d B), cutting off and "
 				       "terminating connection.\n", bytenum, attrib->limitnum);
 			break;
 		}
-	}
+	} /* end of socket reading loop */
+
 	if(log_perr(fderror, "read", "Error reading from socket."))
 		return 1;
 
@@ -195,7 +205,6 @@ int copris_read_socket(int *parentfd, struct Attribs *attrib) {
 
 	if(log_info())
 		log_date();
-
 	if(log_err()) {
 		printf("End of stream, received %d byte(s) in %d chunk(s)",
 			   bytenum, (bytenum && bytenum < BUFSIZE) ? 1 : bytenum / BUFSIZE);