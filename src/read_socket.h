9f203ebf5e570a096b95670bae308651c1863679
bertronika <nejc@bertoncelj.eu.org>
2022-04-24 14:06:17 +0200

    Rename remaining names in read_socket.h

--- src/read_socket.h
+++ src/read_socket.h
@@ -1,11 +1,10 @@
-#ifndef SERVER_H
-#define SERVER_H
+#ifndef SOCKET_H
+#define SOCKET_H
 
-#include <stddef.h>   /* size_t    */
 #include <stdbool.h>  /* bool      */
 #include <utstring.h> /* UT_string */
 
 bool copris_socket_listen(int *parentfd, unsigned int portno);
 bool copris_handle_socket(UT_string *copris_text, int *parentfd, struct Attribs *attrib);
 
-#endif /* SERVER_H */
+#endif /* SOCKET_H */
