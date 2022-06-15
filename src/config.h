e927c39b4fa2def90f0f707df8d59c330fe52475
bertronika <nejc@bertoncelj.eu.org>
2022-06-16 00:25:02 +0200

    Improve config.h commentary

--- src/config.h
+++ src/config.h
@@ -5,16 +5,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Inbound message buffer size
-//   Defines how big each received chunk will be, either from
-//   a socket or from stdin. Number includes the NUL terminator.
+// Inbound text buffer size
 #ifndef BUFSIZE
-#	define BUFSIZE 16
+#   define BUFSIZE 16
 #endif
 
 // Maximum lengths of name and value strings in .ini files
 #define MAX_INIFILE_ELEMENT_LENGTH 32
 
+// Default predefined values for printer feature set commands
 #define P_LIST_ITEM '+'
 
 #endif /* CONFIG_H */
