d83ab9e5a3d8055f1b53c831dca19b38971337f7
bertronika <nejc@bertoncelj.eu.org>
2022-06-21 00:15:57 +0200

    Rework printchar.sh according to manual

--- printchar.sh
+++ printchar.sh
@@ -1,12 +1,34 @@
-#!/bin/bash
-# Print characters from the following interval.
-# Run as ./printchar.sh > /dev/ttyUSB0, according to your printer.
+#!/bin/sh
+# Print listing in the following format to standard output, picking all
+# printable characters:
+#
+#   hex_value (dec_value): 'character'  hex_value (dec_value): 'character' ...
+#
+# Run as ./printchar.sh [OPTIONAL_START_NUM] [OPTIONAL_END_NUM] > /dev/ttyUSB0,
+# according to your output device. Confirmation text is sent to standard error
+# and will thus not appear in the redirected output.
 
-for i in {121..250} # Values are octal
-do
-	octal=$(printf "%o" $i)
-	printf "$i: \\$octal  "
-	[[ $(($i % 10)) == 0 ]] && printf "\n"
+START_VALUE=${1:-32}
+END_VALUE=${2:-255}
+BREAK_AFTER=5
+
+if [ $# -le 1 ]; then
+	printf "Printing values between %d and %d.
+Override with %s [START_NUM] [END_NUM], pass optional values in decimal.\n
+Press ENTER to continue or Ctrl-C to abort.\n" \
+	$START_VALUE $END_VALUE "$0" >&2
+	read
+fi
+
+i=$START_VALUE
+
+while [ $i -le $END_VALUE ]; do
+	# Skip DEL
+	if [ $i != 127 ]; then
+		printf "0x%X (%3d): '\x$(printf '%x' $i)'  " $i $i
+		[ $((i % BREAK_AFTER)) = 0 ] && printf "\n"
+	fi
+	i=$((i+1))
 done
 
 printf "\n"
