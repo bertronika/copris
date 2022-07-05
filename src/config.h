73b3e23b995401891152a7e2716ef9ad6cb898fa
bertronika <nejc@bertoncelj.eu.org>
2022-07-05 17:55:22 +0200

    Improve config.h commentary

--- src/config.h
+++ src/config.h
@@ -5,19 +5,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Inbound text buffer size
+// Inbound text buffer size. This value is used by default in COPRIS,
+// only unit tests override it.
 #ifndef BUFSIZE
 #   define BUFSIZE 16
 #endif
 
-// Maximum lengths of name and value strings in .ini files
+// Maximum lengths of elements - name and value strings - in .ini files
 #define MAX_INIFILE_ELEMENT_LENGTH 32
 
 /*
  * Default predefined values for printer feature set commands.
  *
  * They should be defined in the same format as the .ini files.
- * NOTE: if set improperly, an errpr will be thrown at run time, but only
+ * NOTE: if set improperly, an error will be thrown at run time, but only
  *       with '-r/--printer' or '--dump-commands' specified in the command line.
  */
 #define P_LIST_ITEM "0x3D 0x3E"
