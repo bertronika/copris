f6c1b8edba511c8a16d7532bcfa74e5a06e55640
bertronika <nejc@bertoncelj.eu.org>
2022-07-09 19:54:47 +0200

    Implement setup, teardown functions in unit tests

--- tests/tests_cmocka.c
+++ tests/tests_cmocka.c
@@ -17,6 +17,8 @@
 int verbosity = 0;
 
 // =========================== parse_value.c
+#define CALL_PARSE(value) \
+        parse_value_to_binary(value, parsed_value, (sizeof parsed_value) - 1)
 
 static void parse_value_correct(void **state)
 {
@@ -33,18 +35,36 @@ static void parse_value_erroneous(void **state)
 	(void)state;
 	char parsed_value[MAX_INIFILE_ELEMENT_LENGTH];
 
-	int count = CALL_PARSE("0102 10P1");
-	assert_int_equal(count, -1);
+	int error = CALL_PARSE("0102 10P1"); // Erroneous 'P'
+	assert_int_equal(error, -1);
 
-	count = CALL_PARSE("0x70 486");
-	assert_int_equal(count, -1);
+	error = CALL_PARSE("0x70 486");      // '486' out of range
+	assert_int_equal(error, -1);
 
-	count = CALL_PARSE("0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74");
-	assert_int_equal(count, -1);
+	error = CALL_PARSE("0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74 0x74");
+	assert_int_equal(error, -1);         // Too many numbers to parse
 }
 
 
 // =========================== read_stdin.c, read_socket.c
+
+// Setup and teardown functions are called before and after all the grouped tests
+static int setup_utstring(void **state)
+{
+	UT_string *copris_text;
+	utstring_new(copris_text);
+
+	*state = copris_text;
+	return 0;
+}
+
+static int teardown_utstring(void **state)
+{
+	UT_string *copris_text = *state;
+	utstring_free(copris_text);
+
+	return 0;
+}
 
 static void expected_stats(size_t sizeof_bytes, int chunks)
 {
@@ -55,10 +75,7 @@ static void expected_stats(size_t sizeof_bytes, int chunks)
 // Read no text from stdin
 static void stdin_read_no_text(void **state)
 {
-	(void)state;
-
-	UT_string *copris_text;
-	utstring_new(copris_text);
+	UT_string *copris_text = *state;
 
 	will_return(__wrap_fgets, NULL); /* Signal an EOF */
 
@@ -67,94 +84,94 @@ static void stdin_read_no_text(void **state)
 
 	assert_true(no_text_read);
 	assert_string_equal(utstring_body(copris_text), "");
-
-	utstring_free(copris_text);
 }
 
 // Read two chunks of ASCII text
 static void read_two_chunks(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input_1[] = "aaaBBBccc";
 	const char input_2[] = "DDD";
 	const char result[]  = "aaaBBBcccDDD";
 
-	TEST_WRAPPED_READ(input_1, input_2, result);
-	TEST_WRAPPED_FGETS(input_1, input_2, result);
+	TEST_HANDLE_SOCKET(input_1, input_2, result);
+	TEST_HANDLE_STDIN(input_1, input_2, result);
 }
 
 // Read a string with a 2-byte multibyte character split between two reads
 static void read_multibyte_char2(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input_1[] = {'a', 'a', 'a', 'B', 'B', 'B', 'c' ,'c', '\xC4', '\0'};
 	const char input_2[] = {'\x8D', '\0'};
 	const char result[]  = "aaaBBBccč";
 
-	TEST_WRAPPED_READ(input_1, input_2, result);
-	TEST_WRAPPED_FGETS(input_1, input_2, result);
+	TEST_HANDLE_SOCKET(input_1, input_2, result);
+	TEST_HANDLE_STDIN(input_1, input_2, result);
 }
 
 // Read a string with a 3-byte multibyte character split between two reads, variant 1
 static void read_multibyte_char3a(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input_1[] = {'a', 'a', 'a', 'B', 'B', 'B', 'c' ,'c', '\xE2', '\0'};
 	const char input_2[] = {'\x82', '\xAC', '\0'};
 	const char result[]  = "aaaBBBcc€";
 
-	TEST_WRAPPED_READ(input_1, input_2, result);
-	TEST_WRAPPED_FGETS(input_1, input_2, result);
+	TEST_HANDLE_SOCKET(input_1, input_2, result);
+	TEST_HANDLE_STDIN(input_1, input_2, result);
 }
 
 // Read a string with a 3-byte multibyte character split between two reads, variant 2
 static void read_multibyte_char3b(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input_1[] = {'a', 'a', 'a', 'B', 'B', 'B', 'c', '\xE2', '\x82', '\0'};
 	const char input_2[] = {'\xAC', '\0'};
 	const char result[]  = "aaaBBBc€";
 
-	TEST_WRAPPED_READ(input_1, input_2, result);
-	TEST_WRAPPED_FGETS(input_1, input_2, result);
+	TEST_HANDLE_SOCKET(input_1, input_2, result);
+	TEST_HANDLE_STDIN(input_1, input_2, result);
 }
 
 // Read a string with a 4-byte multibyte character split between two reads
 static void read_multibyte_char4(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input_1[] = {'a', 'a', 'a', 'B', 'B', 'B', 'c' ,'c', '\xF0', '\0'};
 	const char input_2[] = {'\x9F', '\x84', '\x8C', '\0'};
 	const char result[]  = "aaaBBBcc🄌";
 
-	TEST_WRAPPED_READ(input_1, input_2, result);
-	TEST_WRAPPED_FGETS(input_1, input_2, result);
+	TEST_HANDLE_SOCKET(input_1, input_2, result);
+	TEST_HANDLE_STDIN(input_1, input_2, result);
 }
+
+#define MUST_DISCARD 0x00
 
 // Check if text limit gets applied properly
 static void byte_limit_discard_and_cutoff(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input[]  = "aaaBBBccc";
 	const char result[] = "aaaBB";
 
-	TEST_WRAPPED_READ_LIMITED(input, "", 1, MUST_DISCARD);
-	TEST_WRAPPED_READ_LIMITED(input, result, 5, MUST_CUTOFF);
+	TEST_HANDLE_SOCKET_LIMITED(input, "", 1, MUST_DISCARD);
+	TEST_HANDLE_SOCKET_LIMITED(input, result, 5, MUST_CUTOFF);
 }
 
 // Check if multibyte characters are stripped instead of being partially send through
 static void byte_limit_discard_and_cutoff2(void **state)
 {
-	(void)state;
+	UT_string *copris_text = *state;
 	const char input1[] = "abcč"; // strlen("abcč") == 5
 	const char result1[] = "abc";
 
 	const char input2[] = "aaBBcc€"; // strlen("aaBBcc€") == 9
 	const char result2[] = "aaBBcc";
 
-	TEST_WRAPPED_READ_LIMITED(input1, "", 1, MUST_DISCARD);
-	TEST_WRAPPED_READ_LIMITED(input1, result1, 4, MUST_CUTOFF);
-	TEST_WRAPPED_READ_LIMITED(input2, result2, 8, MUST_CUTOFF);
+	TEST_HANDLE_SOCKET_LIMITED(input1, "", 1, MUST_DISCARD);
+	TEST_HANDLE_SOCKET_LIMITED(input1, result1, 4, MUST_CUTOFF);
+	TEST_HANDLE_SOCKET_LIMITED(input2, result2, 8, MUST_CUTOFF);
 }
 
 
@@ -216,6 +233,8 @@ static void utf8_test_incomplete_buffer(void **state)
 }
 
 // ===========================
+#define EXIT_ON_ERROR \
+	    if (error) { return error; }
 
 int main(int argc, char **argv)
 {
@@ -250,11 +269,11 @@ int main(int argc, char **argv)
 	error = cmocka_run_group_tests_name("Test parse_value.c", t_parse_value, NULL, NULL);
 	EXIT_ON_ERROR;
 
-	error = cmocka_run_group_tests_name("Test read_socket.c and read_stdin.c",
-	                                    t_socket_stdin, NULL, NULL);
+	error = cmocka_run_group_tests_name("Test utf8.c", t_utf8, NULL, NULL);
 	EXIT_ON_ERROR;
 
-	error = cmocka_run_group_tests_name("Test utf8.c", t_utf8, NULL, NULL);
+	error = cmocka_run_group_tests_name("Test read_socket.c and read_stdin.c",
+	                                    t_socket_stdin, setup_utstring, teardown_utstring);
 	EXIT_ON_ERROR;
 
 	return error;
--- tests/tests_cmocka.h
+++ tests/tests_cmocka.h
@@ -3,10 +3,7 @@
 #	error "Buffer size set improperly for unit tests."
 #endif
 
-#define CALL_PARSE(value) \
-        parse_value_to_binary(value, parsed_value, (sizeof parsed_value) - 1)
-
-#define TEST_WRAPPED_READ(input_1, input_2, result)              \
+#define TEST_HANDLE_SOCKET(input_1, input_2, result)             \
     do {                                                         \
         int parentfd = 0;                                        \
                                                                  \
@@ -14,9 +11,6 @@
         attrib.daemon = false;                                   \
         attrib.limitnum = 0;                                     \
         attrib.copris_flags = 0x00;                              \
-                                                                 \
-        UT_string *copris_text;                                  \
-        utstring_new(copris_text);                               \
                                                                  \
         will_return(__wrap_read, input_1);                       \
         will_return(__wrap_read, input_2);                       \
@@ -27,10 +21,10 @@
         assert_false(error);                                     \
         assert_string_equal(utstring_body(copris_text), result); \
                                                                  \
-        utstring_free(copris_text);                              \
+        utstring_clear(copris_text);                             \
     } while (0);
 
-#define TEST_WRAPPED_READ_LIMITED(input, result, limit, flags)   \
+#define TEST_HANDLE_SOCKET_LIMITED(input, result, limit, flags)  \
     do {                                                         \
         int parentfd = 0;                                        \
                                                                  \
@@ -39,9 +33,6 @@
         attrib.limitnum = limit;                                 \
         attrib.copris_flags = flags;                             \
                                                                  \
-        UT_string *copris_text;                                  \
-        utstring_new(copris_text);                               \
-                                                                 \
         will_return(__wrap_read, input);                         \
                                                                  \
         int error = copris_handle_socket(copris_text, &parentfd, &attrib); \
@@ -49,14 +40,11 @@
         assert_false(error);                                     \
         assert_string_equal(utstring_body(copris_text), result); \
                                                                  \
-        utstring_free(copris_text);                              \
+        utstring_clear(copris_text);                             \
     } while (0);
 
-#define TEST_WRAPPED_FGETS(input_1, input_2, result)             \
+#define TEST_HANDLE_STDIN(input_1, input_2, result)              \
     do {                                                         \
-        UT_string *copris_text;                                  \
-        utstring_new(copris_text);                               \
-                                                                 \
         will_return(__wrap_fgets, input_1);                      \
         will_return(__wrap_fgets, input_2);                      \
         will_return(__wrap_fgets, NULL); /* Signal an EOF */     \
@@ -67,10 +55,5 @@
         assert_false(error);                                     \
         assert_string_equal(utstring_body(copris_text), result); \
                                                                  \
-        utstring_free(copris_text);                              \
+        utstring_clear(copris_text);                             \
     } while (0);
-
-#define MUST_DISCARD 0x00
-
-#define EXIT_ON_ERROR \
-	    if (error) { return error; }
