0cf858e30c36714be2b9802d25985cd0608540f2
bertronika <nejc@bertoncelj.eu.org>
2023-07-27 23:22:27 +0200

    Improve printerset.h commentary

--- src/printerset.h
+++ src/printerset.h
@@ -1,26 +1,32 @@
 /*
- * Load printer set file 'filename' into a hash table, passed by 'prset'.
- * Return 0 on success.
+ * Load printer set file 'filename' into an internal hash table, passed on by 'prset'.
+ * Return 0 on success or negative on failure, together with an error message.
  */
 int load_printer_set_file(const char *filename, struct Inifile **prset);
 
 /*
- * Print out all printer commands, recognised by COPRIS, in an INI-style format.
+ * Print out all known printer commands in an INI-style format to stdout.
+ * Return 0 on success or negative on failure, together with an error message.
  */
 int dump_printer_set_commands(struct Inifile **prset);
 
 /*
- * Unload printer set hash table, passed on by 'prset'.
+ * Unload internal printer set hash table, passed on by 'prset'.
  */
 void unload_printer_set_file(struct Inifile **prset);
 
 /*
- * Prepend and append any session commands to 'copris_text', present in 'prset'. Set 'state'
- * according to the below enumerator.
+ * List of possible internal states that trigger session commands (see function below),
  */
 typedef enum session {
-	SESSION_PRINT,    /* Apply before each text chunk gets printed    */
-	SESSION_STARTUP,  /* Apply command once, when COPRIS starts up    */
-	SESSION_SHUTDOWN  /* Apply command once, before COPRIS shuts down */
+	SESSION_PRINT,    /* A chunk of text is about to get printed  */
+	SESSION_STARTUP,  /* COPRIS is starting up                    */
+	SESSION_SHUTDOWN  /* COPRIS is shutting down                  */
 } session_t;
+
+/*
+ * Prepend and append to 'copris_text' any session commands, passed on from 'prset'.
+ * Session commands are read from printer set file(s) and used for repetitive actions.
+ * They are executed on various 'state's, contained in the above 'session_t' enumerated list.
+ */
 void apply_session_commands(UT_string *copris_text, struct Inifile **prset, session_t state);
