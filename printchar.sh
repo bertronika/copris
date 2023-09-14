33badcb1e5677f54787e62c5fd5dbe9ea348d1d8
bertronika <nejc@bertoncelj.eu.org>
2023-09-14 16:24:20 +0200

    Rework printchar.sh
    - decrease BREAK_AFTER (previously it would overflow an A4 page)
    - use 'seq' for a sequence of input values
    - replace DEL character with a space
    - don't print quotation marks to output
    - use single quotation marks in printf

--- printchar.sh
+++ printchar.sh
@@ -10,7 +10,7 @@
 
 START_VALUE=${1:-32}
 END_VALUE=${2:-255}
-BREAK_AFTER=5
+BREAK_AFTER=4
 
 if [ "${1}x" = "-hx" ] || [ "${1}x" = "--helpx" ]; then
 	sed -n '2,10p' "$0"
@@ -19,24 +19,31 @@ fi
 
 # If script is running through a pipe, omit confirmation message
 if [ $# -le 1 ] && [ -t 1 ]; then
-	printf "Printing values between %d and %d.
+	printf 'Printing values between %d and %d.
 Override with %s [START_NUM] [END_NUM], pass optional values in decimal.\n
-Press ENTER to continue or Ctrl-C to abort.\n" \
-	$START_VALUE $END_VALUE "$0" >&2
+Press ENTER to continue or Ctrl-C to abort.\n' \
+	"$START_VALUE" "$END_VALUE" "$0" >&2
 	read CONFIRMATION
 fi
 
-i=$START_VALUE
-cnt=1
+COUNT=1
 
-while [ $i -le $END_VALUE ]; do
-	# Skip DEL
-	if [ $i != 127 ]; then
-		printf "0x%X (%3d): '\\$(printf '%o' $i)'  " $i $i
-		[ $((cnt % BREAK_AFTER)) = 0 ] && printf "\n"
+for i in $(seq "$START_VALUE" "$END_VALUE"); do
+	if [ "$i" = 127 ]; then
+		# Replace DEL with a space
+		CHAR=" "
+	else
+		# Convert character to octal
+		CHAR="\\$(printf '%o' "$i")"
 	fi
-	i=$((i + 1))
-	cnt=$((cnt + 1))
+
+	printf "0x%X (%3d): ${CHAR}   " "$i" "$i"
+
+	if [ $((COUNT % BREAK_AFTER)) = 0 ]; then
+		printf '\n'
+	fi
+
+	COUNT=$((COUNT + 1))
 done
 
-printf "\n"
+printf '\n'
