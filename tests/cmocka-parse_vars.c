f9b011151e0ca745671155dc83ce3c351cc09e1a
Nejc Bertoncelj <bertronika@mailo.com>
2026-04-26 11:05:04 +0200

    Add unit tests for parse_variables()

--- tests/cmocka-parse_vars.c
+++ tests/cmocka-parse_vars.c
@@ -38,7 +38,7 @@ static void check_parse_modeline(void **state)
 	utstring_free(text);
 }
 
-#define APPLY_TEST(in, ml, out)                  \
+#define APPLY_ML_TEST(in, ml, out)               \
   utstring_printf(text, in);                     \
   apply_modeline(text, ml);                      \
   assert_string_equal(utstring_body(text), out); \
@@ -50,9 +50,47 @@ static void check_apply_modeline(void **state)
 	UT_string *text;
 	utstring_new(text);
 
-	APPLY_TEST("copris disable-md without newline", ML_DISABLE_MD, "");
-	APPLY_TEST("copris enable-vars\nUntouched text.\n", ML_ENABLE_VAR, "Untouched text.\n");
-	APPLY_TEST("copris unknown\nStill omitted.", ML_UNKNOWN, "Still omitted.");
+	APPLY_ML_TEST("copris disable-md without newline", ML_DISABLE_MD, "");
+	APPLY_ML_TEST("copris enable-vars\nUntouched text.\n", ML_ENABLE_VAR, "Untouched text.\n");
+	APPLY_ML_TEST("copris unknown\nStill omitted.", ML_UNKNOWN, "Still omitted.");
+
+	utstring_free(text);
+}
+
+#define APPLY_PARSE_TEST(in, out)                \
+  utstring_printf(text, in);                     \
+  parse_variables(text, &features);              \
+  assert_string_equal(utstring_body(text), out); \
+  utstring_clear(text);
+
+static void check_parse_variables(void **state)
+{
+	(void)state;
+	struct Inifile *features = NULL;
+	UT_string *text;
+	utstring_new(text);
+
+	APPLY_PARSE_TEST("No variable", "No variable");
+	APPLY_PARSE_TEST("$", "$");
+	APPLY_PARSE_TEST("Symbol $ in between", "Symbol $ in between");
+
+	// Variables
+	//  - defined TODO
+
+	//  - undefined
+	APPLY_PARSE_TEST("$ABC\nDEF", "$ABC\nDEF");
+	APPLY_PARSE_TEST("55 $;ENINH\n60 NP\n", "55 $;ENINH\n60 NP\n");
+
+	// Numerical values
+	APPLY_PARSE_TEST("$0x43", "\x43");
+	APPLY_PARSE_TEST("$ 0x43", "$ 0x43");
+
+	// Comments
+	APPLY_PARSE_TEST("$# Comment\n", ""); // New line after a recognised variable should be skipped
+	APPLY_PARSE_TEST("$# Comment",   "");
+	APPLY_PARSE_TEST("$#c1\n$#c2", "");
+	APPLY_PARSE_TEST("$#c3\n$#c4\ntxt", "txt");
+	APPLY_PARSE_TEST("$#c5\n$#c6\ntxt\n", "txt\n");
 
 	utstring_free(text);
 }
@@ -65,7 +103,8 @@ int main(int argc, char **argv)
 
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(check_parse_modeline),
-		cmocka_unit_test(check_apply_modeline)
+		cmocka_unit_test(check_apply_modeline),
+		cmocka_unit_test(check_parse_variables)
 	};
 
 	return cmocka_run_group_tests_name((__FILE__) + 7, tests, NULL, NULL);
